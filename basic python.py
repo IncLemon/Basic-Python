c36f82e164550907347aaf74dd87aff8211eb1e9
Iamunreal <116500256+IncLemon@users.noreply.github.com>
2023-02-12 21:57:10 -0500

    Add files via upload

---
+++ basic python.py
@@ -0,0 +1,11 @@
+#THIS CODE WILL BE THE MOST BASIC CODE FOR COMPLETE BEGGINERS
+print("Heyyyy")
+#That is a basic print command it will show what you put in there in the terminal
+#
+#Is used to make a comment on your code
+import random
+#that is used to import libarys like Random
+import random
+n = random.randrange(1,10)
+print(n)
+#that will pick a number 1,10 when you have import random imported
